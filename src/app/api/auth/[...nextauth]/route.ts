b7ecb32d6eea62b0da407d0a2d6ef49f1a3b1df4
L2H.working <lhhoang98197@gmail.com>
2024-01-18 01:09:27 +0700

    setup signup page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
 import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    Credentials({
-      name: "Credentials",
-    }),
+    // Credentials({
+    //   name: "Credentials",
+    // }),
   ],
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
